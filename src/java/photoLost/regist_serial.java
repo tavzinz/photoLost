530970af028298672f50a48ea2f7fbb96029eb28
Ricardo <Ricardo@portatil_win8>
2014-02-15 17:01:47 +0000

    deploy para o rpi, corrigido para bd no localhost credenciais da bd retiradas do código para um ficheiro à parte.

--- src/java/photoLost/regist_serial.java
+++ src/java/photoLost/regist_serial.java
@@ -4,13 +4,17 @@
  */
 package photoLost;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Properties;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -57,8 +61,15 @@ public class regist_serial extends HttpServlet {
 	
         try {
             //leitura das credenciais no ficheiro
-            String user = "photolost";
-            String pass = "adminlabredes13";
+            Properties prop = new Properties();
+            InputStream input = null;
+            File file = new File("../../../home/pi/photolost/db");
+            
+            input = new FileInputStream(file);
+            prop.load(input);
+            
+            String user = prop.getProperty("dbuser");
+            String pass = prop.getProperty("dbpassword");
             
             //connecção à BD
             Class.forName("com.mysql.jdbc.Driver");
--- src/java/photoLost/search_serial.java
+++ src/java/photoLost/search_serial.java
@@ -5,12 +5,16 @@ package photoLost;
  * and open the template in the editor.
  */
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -42,8 +46,15 @@ public class search_serial extends HttpServlet {
         try {
             
             //leitura das credenciais no ficheiro
-            String user = "photolost";
-            String pass = "adminlabredes13";
+            Properties prop = new Properties();
+            InputStream input = null;
+            File file = new File("../../../home/pi/photolost/db");
+
+            input = new FileInputStream(file);
+            prop.load(input);
+            
+            String user = prop.getProperty("dbuser");
+            String pass = prop.getProperty("dbpassword");
             
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://192.168.228.242:3306/photolost", user, pass);
